010ec0ce7a35ec4e3e077d4b58eeab27978bb4df
AfekhikuDonald <afekhikudonald@yahoo.com>
2022-09-08 06:54:16 -0700

    alx-low_level_programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+
+/**
+ * main - this prints the required statement
+ * return - this returns 0 if exited properly, otherwise an error occured
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
