0b37eb4ed76e6508f19cba2dd713e332d35596c9
AfekhikuDonald <afekhikudonald@yahoo.com>
2022-09-08 10:10:49 -0700

    alx-low_level_programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - print the required phrase
  * Return: Always 0(success)
  */
-int main(void)
+int main()
 {
-	char ch = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	putchar(ch);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
