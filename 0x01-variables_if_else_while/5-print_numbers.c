aa56e54a56020b717cfd57c442d5529d550b4962
AfekhikuDonald <afekhikudonald@yahoo.com>
2022-09-09 09:36:35 -0700

    variables, if, else, while

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -6,13 +6,10 @@
  */
 int main(void)
 {
-	char n = 0;
+	int n;
 
-	while (n < 10)
-	{
-		putchar(n);
-	}
-	n++;
-	putchar('\n');
+	for (n = 0; n < 10; n++ )
+		printf("%d", n);
+	printf("\n");
 	return (0);
 }
