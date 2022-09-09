9744b4b26b3d0cd3378d5be9e00af60acacd14a0
AfekhikuDonald <afekhikudonald@yahoo.com>
2022-09-09 14:20:39 -0700

    variables, if, else, while

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints lowercase alphabet in reverse
+ *
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch >= 'a')
+		putchar(ch);
+	ch--;
+	putchar('\n');
+
+	return (0);
+}
