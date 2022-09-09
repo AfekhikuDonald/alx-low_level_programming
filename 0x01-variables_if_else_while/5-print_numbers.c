2cefe4f919fcd6c7498ee23f306d4bd5e8e31993
AfekhikuDonald <afekhikudonald@yahoo.com>
2022-09-09 09:17:44 -0700

    variables, if, else, while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print all single digit of number base 10
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+	char n = 0;
+	while (n < 10)
+	{
+		putchar(n);
+	}
+	n++;
+	return (0);
+}
