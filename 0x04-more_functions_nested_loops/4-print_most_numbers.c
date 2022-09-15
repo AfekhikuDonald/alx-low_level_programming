8a4d60554a086f1651ac8b49ac1ee2288ac3c223
AfekhikuDonald <afekhikudonald@yahoo.com>
2022-09-15 08:09:41 -0700

    functions and nested loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,12 +2,14 @@
 
 /**
  * print_most_numbers - prints numbers from 0 to 9 except 2 and 4
+ * Return: void
  */
 void print_most_numbers(void)
 {
 	int i;
 
 	i = '0';
+
 	while (i < '9')
 	{
 		if (i != '2' && i != '4')
