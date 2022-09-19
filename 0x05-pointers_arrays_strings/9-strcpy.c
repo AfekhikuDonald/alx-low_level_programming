adc2631c4c1b99309fa1e84f5a050ae1426f248b
AfekhikuDonald <afekhikudonald@yahoo.com>
2022-09-19 04:24:27 -0700

    pointers,arrays,and strings

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,11 +2,11 @@
 
 /**
  * *_strcpy - copies the string pointed to by src including the terminating null byte to the buffer pointed to by dest
- * @*dest: destination address
- * @*src: source address
+ * @dest: destination address
+ * @src: source address
  * Return: the pointer to dest
  */
-char *_strcpy(char *dest, char *src)
+char *_strcpy(char *dest, const char *src)
 {
 	int i = 0;
 
