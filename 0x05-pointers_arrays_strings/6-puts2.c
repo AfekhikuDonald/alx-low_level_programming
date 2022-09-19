282b470c8e1f4917f8da820d56f86ae9d1315041
AfekhikuDonald <afekhikudonald@yahoo.com>
2022-09-19 03:08:03 -0700

    pointers,arrays,and strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string starting from 0 followed by a new line
+ * @str: the string to be reversed
+ */
+void puts2(char *str)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+	while (str[i++])
+	{
+		j++;
+	}
+	for (i = 0; i < j; i+= 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
