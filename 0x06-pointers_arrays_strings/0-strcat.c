9cf8c2205dce8c251554bffa1dce010efb9c8b2b
AfekhikuDonald <afekhikudonald@yahoo.com>
2022-09-21 06:10:46 -0700

    pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcat - appends a string
+ * @dest: destination string
+ * @src: source string
+ * Return: pointer to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int l1, l2, i;
+
+	for (dest[l1])
+		li++;
+	for (src[l2])
+		l2++;
+	for (i = 0; i <= l2; i++)
+		dest[l1 + i] = src[i];
+		_putchar(dest);
+}
