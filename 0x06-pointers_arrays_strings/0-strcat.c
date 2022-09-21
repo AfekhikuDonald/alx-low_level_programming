140e36ffeea623c62387db1f8dd65a6b4301753b
AfekhikuDonald <afekhikudonald@yahoo.com>
2022-09-21 07:21:08 -0700

    pointers_arrays_strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,11 +10,15 @@ char *_strcat(char *dest, char *src)
 {
 	int l1, l2, i;
 
-	for (dest[l1])
-		li++;
-	for (src[l2])
+	l1 = l2 = 0;
+
+	while (dest[l1])
+		l1++;
+	while (src[l2])
 		l2++;
 	for (i = 0; i <= l2; i++)
+	{
 		dest[l1 + i] = src[i];
-		_putchar(dest);
+	}
+	return (dest);
 }
