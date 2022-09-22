1818ad3134367951b47e5779531ff2800c90e1c5
AfekhikuDonald <afekhikudonald@yahoo.com>
2022-09-22 10:06:06 -0700

    pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: string
+ * Return: pointer to string
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		for (str[i] >= 'a' && str[i] <= 'z')
+		{
+			if (str[i - 1] == ',' ||
+			    str[i - 1] == ';' ||
+			    str[i - 1] == '.' ||
+			    str[i - 1] == '!' ||
+			    str[i - 1] == '?' ||
+			    str[i - 1] == '"' ||
+			    str[i - 1] == '(' ||
+			    str[i - 1] == ')' ||
+			    str[i - 1] == '{' ||
+			    str[i - 1] == '}' ||
+			    i == 0)
+				str[i] -= 32;
+		}
+	return (str);
+}
