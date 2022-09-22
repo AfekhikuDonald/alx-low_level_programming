c7a4af0a8db5e45f8584f9457e62998909605072
AfekhikuDonald <afekhikudonald@yahoo.com>
2022-09-22 09:05:15 -0700

    pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @str: string to be encoded
+ * Return: pointer to str
+ */
+char *leet(char *str)
+{
+	int i, j;
+
+	char c[10] = {'o', 'O', 'e', 'E', 'a', 'A', 't', 'T', 'l', 'L'}
+	;char sw[10] = {'0', '0', '3', '3', '4', '4', '7', '7', '1', '1'}
+
+	;i = j = 0;
+	for (i = 0; str[i] != '\0'; i++)
+		for (j = 0; j < 10; j++)
+		{
+			if (str[i] == c[j])
+			{
+				str[i] = sw[j];
+			}
+		}
+	return (str);
+}
