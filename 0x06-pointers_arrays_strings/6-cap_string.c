f3f489ec566af0264a47cedf7c72b9db80ee7fb4
AfekhikuDonald <afekhikudonald@yahoo.com>
2022-09-22 10:26:44 -0700

    pointers_arrays_strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,20 +10,22 @@ char *cap_string(char *str)
 	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
-		for (; str[i] >= 'a' && str[i] <= 'z'; i++)
+	{
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
 		{
-			if (str[i - 1] == ',' ||
-			    str[i - 1] == ';' ||
-			    str[i - 1] == '.' ||
-			    str[i - 1] == '!' ||
-			    str[i - 1] == '?' ||
-			    str[i - 1] == '"' ||
-			    str[i - 1] == '(' ||
-			    str[i - 1] == ')' ||
-			    str[i - 1] == '{' ||
-			    str[i - 1] == '}' ||
-			    i == 0)
-				str[i] -= 32;
+			i++;
 		}
+		if (str[i - 1] == ',' ||
+		    str[i - 1] == ';' ||			    str[i - 1] == '.' ||
+		    str[i - 1] == '!' ||
+		    str[i - 1] == '?' ||
+		    str[i - 1] == '"' ||
+		    str[i - 1] == '(' ||
+		    str[i - 1] == ')' ||
+		    str[i - 1] == '{' ||
+		    str[i - 1] == '}' ||
+		    i == 0)
+			str[i] -= 32;
+	}
 	return (str);
 }
