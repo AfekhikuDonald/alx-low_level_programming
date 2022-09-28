c608560ef6ec25411d752e0ec5b52e98f53c6844
root <root@kali>
2022-09-28 14:34:19 -0400

    recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,12 +3,13 @@
 /**
  * _strlen_recursion - returns the lenght of a string
  * @s: string to be checked
+ * Return: lenght of the string
  */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		return(0);
+		return (0);
 	}
 	else
 	return (1 + _strlen_recursion(s + 1));
