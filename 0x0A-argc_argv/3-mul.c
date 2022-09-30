ee6b3a44b5b2b791068ac316ea1ef6b338786dd4
root <root@kali>
2022-09-30 12:39:31 -0400

    argc, argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,16 +12,13 @@ int main(int argc, char *argv[])
 	int i, j;
 
 
-	if (argc == 2)
+	if (argc == 3)
 	{
 		i = atoi(argv[1]);
 		j = atoi(argv[2]);
 		printf("%d\n", i * j);
 		return (0);
 	}
-	else
-	{
-		printf("Error\n");
-		return (1);
-	}
+	printf("Error\n");
+	return (1);
 }
