8a1cb9553060b0cdc19e488f9c222b1475a7f86f
AfekhikuDonald <afekhikudonald@yahoo.com>
2022-10-04 04:11:45 -0700

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,22 +10,22 @@
  */
 char *create_array(unsigned int size, char c)
 {
-        char *arr;
-        unsigned int i;
+	char *arr;
+	unsigned int i;
 
-        if (size == 0)
-                return (NULL);
-        arr = (char *) malloc(sizeof(c) * size);
+	if (size == 0)
+		return (NULL);
+	arr = (char *) malloc(sizeof(c) * size);
 
-        if (arr == 0)
-                return (NULL);
-        else
-        {
-                for (i = 0; i < size; i++)
-                {
-                        *(arr + i ) = c;
-                }
-        return (arr);
-        }
-        free (arr);
+	if (arr == 0)
+		return (NULL);
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			*(arr + i ) = c;
+		}
+	return (arr);
+	}
+	free (arr);
 }
