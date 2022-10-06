96f660907493ad8d5b81cd75430d2068ca7a5fa3
AfekhikuDonald <afekhikudonald@yahoo.com>
2022-10-06 07:01:33 -0700

    more malloc tasks

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -40,6 +40,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	s3 = (char *) malloc(sizeof(s3) * size);
 
+	if (s3 == NULL)
+		return (NULL);
 	if (n >= size2)
 		n = size2;
 	for (i = 0; s1[i] != '\0'; i++)
