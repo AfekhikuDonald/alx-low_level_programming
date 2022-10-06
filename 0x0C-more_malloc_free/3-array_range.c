8fd605776043b203267d8e9bdb96b80bdea67d40
AfekhikuDonald <afekhikudonald@yahoo.com>
2022-10-06 09:21:19 -0700

    more malloc tasks

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,7 +14,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	size = max - min + 1; 
+	size = max - min + 1;
 	array = (int *) malloc(sizeof(int) * size);
 	if (array == NULL)
 		return (NULL);
