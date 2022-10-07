a8c790b82853c65b443bf2d68b91f38478e6c8a3
AfekhikuDonald <afekhikudonald@yahoo.com>
2022-10-06 17:07:11 -0700

    more malloc tasks

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,7 +13,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	array = malloc(size * (nmemb * size));
+	array = malloc(size * (nmemb * size + 1));
 	if (array == NULL)
 		return (NULL);
 	return (array);
