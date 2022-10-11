2eabed0b091266dd32a6d27a86fff0f8cb992fa0
AfekhikuDonald <afekhikudonald@yahoo.com>
2022-10-10 17:36:37 -0700

    structures typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Description: description
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	 char *owner;
+};
+#endif
