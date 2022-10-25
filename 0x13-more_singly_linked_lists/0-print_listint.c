c8b6543e9cc0ae48693e850c717a94a1055991e0
root <root@kali>
2022-10-25 13:26:34 -0400

    more singly linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,8 +2,7 @@
 
 /**
  * print_listint - prints all the elements of a linked list
- * @h: linked list of type listint_t 
- *
+ * @h: linked list of type listint_t
  * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
