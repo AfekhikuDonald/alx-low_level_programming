e0f8e261fd5967bf0fa07171a65be4c4c5ba6ab4
root <root@kali>
2022-10-25 13:13:48 -0400

    more singly linked lists

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,12 +8,12 @@
  */
 size_t listint_len(const listint_t *h)
 {
-    size_t i = 0;
+	size_t i = 0;
 
-    while (h)
-    {
-        i++;
-        h = h->next;
-    }
-    return (i);
+	while (h)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
 }
