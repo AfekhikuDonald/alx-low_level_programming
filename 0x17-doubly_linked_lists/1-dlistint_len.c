1973ed2807bb83428efcc288d6901194320e3a58
AfekhikuDonald <afekhikudonald@yahoo.com>
2022-12-08 05:42:30 -0800

    doubly linked lists

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the lenght of the list
+ * @h: doubly linked list
+ * Return: lenght of the list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	dlistint_t tmp = h;
+	size_t len = 0;
+
+	while (tmp != NULL)
+	{
+		len++;
+	}
+	return (len);
+}
