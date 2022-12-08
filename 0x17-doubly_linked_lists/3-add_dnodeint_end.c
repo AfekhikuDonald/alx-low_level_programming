f182ea2c63db4f2af6a3d9f51e753a3b3a4bbc00
AfekhikuDonald <afekhikudonald@yahoo.com>
2022-12-08 07:35:44 -0800

    doubly linked lists

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -18,7 +18,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	end_node->next = NULL;
 	if (*head == NULL)
 	{
-		*head= end_node;
+		*head = end_node;
 		return (*head);
 	}
 	temp = *head;
