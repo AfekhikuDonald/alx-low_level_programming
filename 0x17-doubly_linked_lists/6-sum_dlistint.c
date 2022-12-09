fb26b361362acee9e8e715a3cdab98d8a906243b
AfekhikuDonald <afekhikudonald@yahoo.com>
2022-12-08 16:12:22 -0800

    doubly linked lists

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -8,16 +8,11 @@
 int sum_dlistint(dlistint_t *head)
 {
 	int sum = 0;
-	dlistint_t *temp;
 
-	if (head == NULL)
-		return (0);
-
-	temp = head;
 	while (head != NULL)
 	{
-		sum += temp->n;
-		temp = temp->next;
+		sum += head->n;
+		head = head->next;
 	}
 	return (sum);
 }
