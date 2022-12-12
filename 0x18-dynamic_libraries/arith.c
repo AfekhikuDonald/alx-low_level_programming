d7b7a9d22ed746a67600ad890364070233e780e0
AfekhikuDonald <afekhikudonald@yahoo.com>
2022-12-12 10:25:13 -0500

    dynamic libraries

---
+++ 0x18-dynamic_libraries/arith.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+
+/**
+ * add - sums two numbers
+ * @a: number
+ * @b: number
+ * Return: result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ * sub - subtracts two numbers
+ * @a: number
+ * @b: number
+ * Return: result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+/**
+ * mul - multiplies two numbers
+ * @a: number
+ * @b: number
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+ * div - divides two numbers
+ * @a: number
+ * @b: number
+ * Return: result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+
+/**
+ * mod - finds the modulos of two numbers
+ * @a: number
+ * @b: number
+ * Return: result
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
