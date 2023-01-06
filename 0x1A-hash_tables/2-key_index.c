33d315dfee86d56e67df2b48e7b3c665c6d68505
AfekhikuDonald <afekhikudonald@yahoo.com>
2023-01-06 16:40:51 -0500

    hash tables

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -7,6 +7,6 @@
  * Return: the index
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size) {
-    unsigned long int hash = hash_djb2(key);
-    return hash % size;
+	unsigned long int hash = hash_djb2(key);
+	return hash % size;
 }
