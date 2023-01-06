5994d422b6ab3b9948c8a2193c4f217c4a42af72
AfekhikuDonald <afekhikudonald@yahoo.com>
2023-01-06 17:25:48 -0500

    hash tables

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -4,7 +4,7 @@
  * hash_table_get - retrieves a value associated with a key
  * @ht: hash table
  * @key: key to look into
- * Return:  the value associated with the element, or NULL if key couldn’t be found
+ * Return:  the value associated with the element, or NULL
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
@@ -14,9 +14,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	index = key_index((unsigned char *)key, ht->size);
 	current = ht->array[index];
 	while (current != NULL)
-       	{
+	{
 		if (strcmp(current->key, key) == 0)
-	       	{
+		{
 			return (current->value);
 		}
 		current = current->next;
