70531c77e271e59d0cf3eaba62aab26f593ffaed
AfekhikuDonald <afekhikudonald@yahoo.com>
2023-01-06 17:11:53 -0500

    hash tables

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,25 +11,22 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new_node;
 	unsigned long int index;
-    
-	if (strlen(key) == 0) {
+
+	if (strlen(key) == 0)
 		return (0);
-	}
 	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL) {
+	if (new_node == NULL)
 		return (0);
-	}
+
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
 	new_node->next = NULL;
 
 	index = key_index((unsigned char *)key, ht->size);
 	if (ht->array[index] == NULL)
-       	{
 		ht->array[index] = new_node;
-	}
-       	else
-       	{
+	else
+	{
 		new_node->next = ht->array[index];
 		ht->array[index] = new_node;
 	}
